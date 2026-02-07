5f505b1927b7c44cbbec26ff8edd00f62fc6e86d
Muhaimin Saad <201452209+muhaiminsaad@users.noreply.github.com>
2026-02-07 13:02:27 +0600

    Create LinearSearch.java

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,20 @@
+public class LinearSearch{
+    public static void main(String[] args){
+        int[] data={1,2,7,4,5,6,7,8,9,7};
+        LinearSearch(data,7);
+    }
+
+    static void LinearSearch(int[] array,int key){
+        boolean isFound=false;
+        for(int i=0;i<array.length;i++){
+            if(array[i]==key){
+                System.out.println("Found at position "+(i+1));
+                isFound=true;
+                break;
+            }
+        }
+        if(!isFound){
+            System.out.println("Not found");
+        }
+    }
+}
