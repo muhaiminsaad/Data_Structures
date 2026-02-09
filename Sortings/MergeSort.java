f663ec98136643c1d38159746e4ac0b87cbb57e9
Muhaimin Saad <201452209+muhaiminsaad@users.noreply.github.com>
2026-02-10 01:15:29 +0600

    Update MergeSort.java

--- Sortings/MergeSort.java
+++ Sortings/MergeSort.java
@@ -1,21 +1,23 @@
 public class MergeSort {
     public static void main(String[] args) {
-        int[] data={12,1,35,44,21,56,23,80,64};
-        MergeSort(data);
+        int[] data={45,23,11,78,432,13,97,58};
+        Sort.MergeSort(data);
 
         for(int x:data){
             System.out.print(x+" ");
         }
     }
-
+    //this is the entry point for merge sorting, and it should not be private
     static void MergeSort(int[] array){
         int length=array.length;
+        //halting condition of recursion
         if(length<=1){
             return;
         }
         int mid=length/2;
-        int[] leftArray= new int[mid];
-        int[] rightArray= new int[length-mid];
+
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length-mid];
 
         int i=0,j=0;
 
@@ -24,41 +26,49 @@ public class MergeSort {
                 leftArray[i]=array[i];
             }
             else{
-                rightArray[j]=array[i];
-                j++;
+                rightArray[j++]=array[i];
             }
         }
+
+        //Display splitting of arrays in each recursion>>
+        /*
+        for(int x: leftArray){
+            System.out.print(x+" ");
+        }
+
+        System.out.print("| ");
+
+        for(int y: rightArray){
+            System.out.print(y+" ");
+        }
+        System.out.println("");
+        */
+
         MergeSort(leftArray);
         MergeSort(rightArray);
         Merge(leftArray,rightArray,array);
     }
-
-    static void Merge(int[] leftArray,int[] rightArray,int[] array){
+    //Helper methods should be private, entry-point methods (MergeSort) should not be private
+    //Merge() is a part of MergeSort(), so running Merge() makes no sense
+    private static void Merge(int[] leftArray,int[] rightArray,int[] array){
         int leftSize=leftArray.length;
         int rightSize=rightArray.length;
 
         int i=0,l=0,r=0;
 
         while(l<leftSize && r<rightSize){
-            if(leftArray[l]<rightArray[r]){
-                array[i]=leftArray[l];
-                l++;
+            if(leftArray[l]<=rightArray[r]){
+                array[i++]=leftArray[l++];
             }
             else{
-                array[i]=rightArray[r];
-                r++;
+                array[i++]=rightArray[r++];
             }
-            i++;
         }
         while(l<leftSize){
-            array[i]=leftArray[l];
-            i++;
-            l++;
+            array[i++]=leftArray[l++];
         }
         while(r<rightSize){
-            array[i]=rightArray[r];
-            i++;
-            r++;
+            array[i++]=rightArray[r++];
         }
     }
 }
